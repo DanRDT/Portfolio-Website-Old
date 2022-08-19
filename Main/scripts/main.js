6404e41cb9a00abe835b215ac3d2b495075eccdb
unknown <pulberdan@gmail.com>
2022-08-19 19:44:09 -0400

    appearing logo

--- Main/scripts/main.js
+++ Main/scripts/main.js
@@ -1,15 +1,37 @@
+// Contact Form
 document.addEventListener("click", e => { 
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
-    
-    let currentDropdown
-    if (isDropdownButton) {
-      currentDropdown = e.target.closest("[data-dropdown]")
-      currentDropdown.classList.toggle("active")
-    }
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
   
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-      if (dropdown === currentDropdown) return
-      dropdown.classList.remove("active")
-    })
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]")
+    currentDropdown.classList.toggle("active")
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove("active")
   })
+})
+
+// Logo
+const sectionOne = document.querySelector("#section1");
+const logo = document.querySelector(".logo");
+
+const options = {
+  rootMargin: "-30%"
+};
+
+const logoObserver = new IntersectionObserver(function(entries, logoObserver) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      logo.classList.toggle('visible');
+    }
+    else {
+      logo.classList.remove('visible')
+    }
+  })
+}, options);
+
+logoObserver.observe(sectionOne);
