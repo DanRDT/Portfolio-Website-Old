55459a580c7b1882fe2286ec05d650b6067eb4f0
unknown <pulberdan@gmail.com>
2022-09-14 16:41:16 -0400

    Scrollbar circles completes

--- scripts/main.js
+++ scripts/main.js
@@ -161,20 +161,50 @@ observedItems.forEach(item => {
 
 //scroll bar
 
-const scrollbarCircles = document.querySelectorAll("[lazy-load]");
+const scrollbarCircles = document.querySelectorAll("section");
+const scrollbarSecCircles = document.querySelectorAll(".scrollbar-circle")
 
 scrollbarObserverOffset = {
-  
+  rootMargin: "-50%"
 };
 
 const scrollbarObserver = new IntersectionObserver( (entries, scrollbarObserver) => {
   entries.forEach(entry => {
     
-
-
+    const isIntersecting = entry.isIntersecting;
+    const scrollbarSec = entry.target.id.substring(7,8) - 1
+    const scrollbarSecCircle = scrollbarSecCircles[scrollbarSec]
+    
+    if (isIntersecting) {
+      scrollbarSecCircle.classList.add('currentSec')
+    }
+    else if (!isIntersecting) {
+      scrollbarSecCircle.classList.remove('currentSec')
+    }
+    
   })
 }, scrollbarObserverOffset);
 
-observedItems.forEach(item => {
+scrollbarCircles.forEach(item => {
   scrollbarObserver.observe(item);
 });
+
+
+// Scroll functionality without changing url
+document.querySelectorAll('a').forEach(anchor => {
+  
+  const pageSectionRedirect = anchor.getAttribute('href')
+
+  //check if it redirects to on the page
+  if (pageSectionRedirect.substring(0,1) !== "#") {
+    return;
+  }
+
+  anchor.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(pageSectionRedirect).scrollIntoView(true, {behavior: 'smooth'});
+  });
+
+});
+
+
