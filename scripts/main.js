4ecc6e9f55a4165de8e03fe21f86b4667c918ecd
unknown <pulberdan@gmail.com>
2023-02-09 17:11:43 -0500

    project popup basic css

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,23 @@
 // Dropdowns functionality
-import dropdownEventListener from './dropdowns.js'
+import dropdownEventListener from "./dropdowns.js"
 
 // Logo appears after Section 1
-import logo from './logo.js';
+import logo from "./logo.js"
 
 // Scrolling line
-import scrollLine from "./scrollLine.js"
+// import scrollLine from "./scrollLine.js"
 
 //Clipboard Functionality
-import clipboard from './clipboard.js';
+import clipboard from "./clipboard.js"
 
 //Show Items on Scroll
-import appearOnScroll from './appearOnScroll.js'
+import appearOnScroll from "./appearOnScroll.js"
 
 //Scroll bar on left side of screen
-import scrollbar from './scrollbar.js'
+// import scrollbar from './scrollbar.js'
 
 // Scroll functionality without changing url
-import nonURLScroll from './nonURLScroll.js';
+import nonURLScroll from "./nonURLScroll.js"
 
 // Contact form functionality
-import contactform from './contactform.js';
+import contactform from "./contactform.js"
