b541183bee4c51335187ccab1441b10d3bfe9164
unknown <pulberdan@gmail.com>
2023-02-10 20:42:40 -0500

    popup script fix and project desc

--- scripts/projectPopup.js
+++ scripts/projectPopup.js
@@ -1,16 +1,35 @@
+const body = document.querySelector("body")
+const logo = document.querySelector(".logo")
+
 export default document.querySelectorAll(".project").forEach((projectNode) => {
   const projectPopup = projectNode.lastElementChild
 
   projectNode.addEventListener("click", (e) => {
     const isCloseButton = e.target.closest(".x-button") // returns null or node
 
-    if (isCloseButton) projectPopup.classList.remove("active")
-    else projectPopup.classList.add("active")
+    if (isCloseButton) {
+      hidePopup(projectPopup)
+    } else {
+      showPopup(projectPopup)
+    }
   })
 
   window.addEventListener("click", (e) => {
     const isPartOfProject = e.target.closest(".project") // returns null or node
-
-    if (!isPartOfProject) projectPopup.classList.remove("active")
+    if (!isPartOfProject) {
+      hidePopup(projectPopup)
+    }
   })
 })
+
+function showPopup(popup) {
+  popup.classList.add("active")
+  body.style.overflow = "hidden"
+  logo.style.opacity = "0"
+}
+
+function hidePopup(popup) {
+  popup.classList.remove("active")
+  body.style.overflow = "visible"
+  logo.style.opacity = "1"
+}
